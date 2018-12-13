03cfdb7dbe419471a28734bd52396b62e7290599
Levi Campbell <levicc00123@gmail.com>
2018-12-13 09:31:08 -0700

    Added bak files to gitignore and updated the info file.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
 (define collection "facebook-graph-rk")
-(define deps '("base"))
+(define deps '("base" "request"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/facebook-graph-rk.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Description Here.")
 (define version "0.0")
-(define pkg-authors '(levic))
+(define pkg-authors "Levi Campbell")
